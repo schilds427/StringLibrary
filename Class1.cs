63c7eb57d2cced90261a47b4eb7ef51da7340a63
Steven Childs <sachilds@live.com>
2018-08-24 14:57:00 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UtilityLibraries
+{
+    public static class StringLibaries
+    {
+        public static bool StartsWithUpper(this String str)
+        {
+            if (String.IsNullOrWhiteSpace(str))
+                return false;
+
+            Char ch = str[0];
+            return Char.IsUpper(ch);
+        }
+    }
+}
